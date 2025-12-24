fcb58f1aeef7173955803779ba27fe262cd6ecd1
Luis Eduardo <eduardo.devop@gmail.com>
2025-12-24 00:30:41 -0600

    Add embedded logo assets in ICO and PNG formats

---
+++ assets/embed.go
@@ -0,0 +1,21 @@
+package assets
+
+import _ "embed"
+
+//go:embed logo/ico/logo-black-white.ico
+var LogoBlackWhiteICO []byte
+
+//go:embed logo/ico/logo-black-pink.ico
+var LogoBlackPinkICO []byte
+
+//go:embed logo/ico/logo-black-blue.ico
+var LogoBlackBlueICO []byte
+
+//go:embed logo/png/logo-black-white.png
+var LogoBlackWhitePNG []byte
+
+//go:embed logo/png/logo-black-pink.png
+var LogoBlackPinkPNG []byte
+
+//go:embed logo/png/logo-black-blue.png
+var LogoBlackBluePNG []byte

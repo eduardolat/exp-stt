0f9d8e03a494174c5fe42bcb21b0fa512e395bae
Luis Eduardo <eduardo.devop@gmail.com>
2025-12-25 13:15:03 -0600

    Refactor systray instance to manage status and animation, enhancing icon and title updates

--- internal/systray/systray.go
+++ internal/systray/systray.go
@@ -1,38 +1,200 @@
 package systray
 
 import (
+	"time"
+
 	"fyne.io/systray"
 	"github.com/eduardolat/exp-stt/assets/logo"
 	"github.com/eduardolat/exp-stt/internal/config"
 )
 
+const animationFrameDuration = time.Millisecond * 100
+
+type animationPosition int
+
+const (
+	animationPositionMiddle animationPosition = iota
+	animationPositionRight
+	animationPositionLeft
+)
+
+type status int
+
+const (
+	statusUnloaded status = iota
+	statusLoading
+	statusLoaded
+	statusListening
+	statusTranscribing
+	statusPostProcessing
+)
+
 type Instance struct {
 	systrayStart func()
 	systrayEnd   func()
+
+	statusCurr status
+	statusPrev status
+
+	animationPosCurr animationPosition
+	animationPosPrev animationPosition
+	animationTimer   *time.Timer
+
+	isShuttingDown bool
 }
 
 func New() *Instance {
-	i := &Instance{}
+	i := &Instance{
+		statusCurr:       statusUnloaded,
+		statusPrev:       statusUnloaded,
+		animationPosCurr: animationPositionMiddle,
+		animationPosPrev: animationPositionMiddle,
+		animationTimer:   time.NewTimer(0),
+	}
 
-	systray.SetTitle(config.App.AppName)
-	systray.SetTooltip(config.App.AppName)
-	systray.SetIcon(logo.LogoBlackGray.PNG.Size512.Logo)
-
-	start, end := systray.RunWithExternalLoop(i.onReady, i.onExit)
+	start, end := systray.RunWithExternalLoop(i.onReady, func() {})
 	i.systrayStart = start
 	i.systrayEnd = end
 
 	return i
 }
 
-func (s *Instance) onReady() {}
-
-func (s *Instance) onExit() {}
-
-func (s *Instance) Start() {
-	s.systrayStart()
+func (i *Instance) onReady() {
+	i.SetStatus(statusUnloaded)
+	i.animate()
 }
 
-func (s *Instance) Shutdown() {
-	s.systrayEnd()
+func (i *Instance) Start() {
+	i.systrayStart()
+}
+
+func (i *Instance) Shutdown() {
+	i.isShuttingDown = true
+	i.animationTimer.Stop()
+	i.systrayEnd()
+}
+
+// setNextAnimationPosition advances the animation position.
+//
+// For unloaded and loaded statuses, the animation position is always set to middle, for
+// other statuses, it cycles through middle, right, and left positions.
+func (i *Instance) setNextAnimationPosition() {
+	i.animationPosPrev = i.animationPosCurr
+
+	if i.statusCurr == statusUnloaded || i.statusCurr == statusLoaded {
+		i.animationPosCurr = animationPositionMiddle
+		return
+	}
+
+	switch i.animationPosCurr {
+	case animationPositionMiddle:
+		i.animationPosCurr = animationPositionRight
+	case animationPositionRight:
+		i.animationPosCurr = animationPositionLeft
+	case animationPositionLeft:
+		i.animationPosCurr = animationPositionMiddle
+	}
+}
+
+// SetStatus changes the current status of the systray instance.
+func (i *Instance) SetStatus(newStatus status) {
+	i.statusPrev = i.statusCurr
+	i.statusCurr = newStatus
+
+	i.setTitle()
+	i.setIcon()
+}
+
+// setTitle updates the systray title and tooltip based on the current status.
+func (i *Instance) setTitle() {
+	title := config.App.AppName
+
+	switch i.statusCurr {
+	case statusUnloaded:
+		title += " - Model not loaded"
+	case statusLoading:
+		title += " - Loading model..."
+	case statusLoaded:
+		title += " - Model loaded"
+	case statusListening:
+		title += " - Listening..."
+	case statusTranscribing:
+		title += " - Transcribing..."
+	case statusPostProcessing:
+		title += " - Post-processing..."
+	}
+
+	systray.SetTitle(title)
+	systray.SetTooltip(title)
+}
+
+// setIcon updates the systray icon based on the current status and animation position.
+func (i *Instance) setIcon() {
+	var left, middle, right []byte
+
+	if i.statusCurr == statusUnloaded {
+		left = logo.LogoBlackGray.PNG.Size512.Left
+		middle = logo.LogoBlackGray.PNG.Size512.Middle
+		right = logo.LogoBlackGray.PNG.Size512.Right
+	}
+
+	if i.statusCurr == statusLoading {
+		left = logo.LogoBlackAmber.PNG.Size512.Left
+		middle = logo.LogoBlackAmber.PNG.Size512.Middle
+		right = logo.LogoBlackAmber.PNG.Size512.Right
+	}
+
+	if i.statusCurr == statusLoaded {
+		left = logo.LogoBlackWhite.PNG.Size512.Left
+		middle = logo.LogoBlackWhite.PNG.Size512.Middle
+		right = logo.LogoBlackWhite.PNG.Size512.Right
+	}
+
+	if i.statusCurr == statusListening {
+		left = logo.LogoBlackPink.PNG.Size512.Left
+		middle = logo.LogoBlackPink.PNG.Size512.Middle
+		right = logo.LogoBlackPink.PNG.Size512.Right
+	}
+
+	if i.statusCurr == statusTranscribing {
+		left = logo.LogoBlackBlue.PNG.Size512.Left
+		middle = logo.LogoBlackBlue.PNG.Size512.Middle
+		right = logo.LogoBlackBlue.PNG.Size512.Right
+	}
+
+	if i.statusCurr == statusPostProcessing {
+		left = logo.LogoBlackGreen.PNG.Size512.Left
+		middle = logo.LogoBlackGreen.PNG.Size512.Middle
+		right = logo.LogoBlackGreen.PNG.Size512.Right
+	}
+
+	switch i.animationPosCurr {
+	case animationPositionLeft:
+		systray.SetIcon(left)
+	case animationPositionMiddle:
+		systray.SetIcon(middle)
+	case animationPositionRight:
+		systray.SetIcon(right)
+	}
+}
+
+// animate runs the animation loop, updating the systray icon and title based on the current status
+// and animation position at regular intervals defined by animationFrameDuration.
+func (i *Instance) animate() {
+	for range i.animationTimer.C {
+		if i.isShuttingDown {
+			return
+		}
+
+		if i.statusPrev != i.statusCurr {
+			i.setTitle()
+		}
+
+		if i.statusPrev != i.statusCurr || i.animationPosPrev != i.animationPosCurr {
+			i.setIcon()
+		}
+
+		i.setNextAnimationPosition()
+		i.animationTimer.Reset(animationFrameDuration)
+	}
 }

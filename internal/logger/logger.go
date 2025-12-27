163088a2c83668ebd18caf6b2418b5ca97c8f101
Luis Eduardo <eduardo.devop@gmail.com>
2025-12-26 20:53:15 -0600

    Implement logging interface with slog integration

---
+++ internal/logger/logger.go
@@ -0,0 +1,53 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+	"os"
+)
+
+// Logger is a simple logging interface that supports different log levels.
+type Logger interface {
+	// Info logs an informational message.
+	Info(ctx context.Context, msg string, keysAndValues ...any)
+	// Warn logs a warning message.
+	Warn(ctx context.Context, msg string, keysAndValues ...any)
+	// Error logs an error message.
+	Error(ctx context.Context, msg string, keysAndValues ...any)
+	// Debug logs a debug message. Only logged if debug level is enabled.
+	Debug(ctx context.Context, msg string, keysAndValues ...any)
+}
+
+type slogLogger struct {
+	slogger     *slog.Logger
+	enableDebug bool
+}
+
+// NewStdLogger creates a new text Logger that writes to the standard output.
+func NewSlogLogger(enableDebug bool) Logger {
+	slogger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+
+	return &slogLogger{
+		slogger:     slogger,
+		enableDebug: enableDebug,
+	}
+}
+
+func (l *slogLogger) Info(ctx context.Context, msg string, keysAndValues ...any) {
+	l.slogger.InfoContext(ctx, msg, keysAndValues...)
+}
+
+func (l *slogLogger) Warn(ctx context.Context, msg string, keysAndValues ...any) {
+	l.slogger.WarnContext(ctx, msg, keysAndValues...)
+}
+
+func (l *slogLogger) Error(ctx context.Context, msg string, keysAndValues ...any) {
+	l.slogger.ErrorContext(ctx, msg, keysAndValues...)
+}
+
+func (l *slogLogger) Debug(ctx context.Context, msg string, keysAndValues ...any) {
+	if !l.enableDebug {
+		return
+	}
+	l.slogger.DebugContext(ctx, msg, keysAndValues...)
+}

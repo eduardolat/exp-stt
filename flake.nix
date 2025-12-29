3af4552bb8e1a4ee1a4ccf7edb6ee3f64e630d81
Luis Eduardo <eduardo.devop@gmail.com>
2025-12-28 20:44:09 -0600

    feat: Add urpc derivation and include it in the development shell.

--- flake.nix
+++ flake.nix
@@ -12,6 +12,26 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+
+      # UFO RPC
+      urpc = pkgs.stdenv.mkDerivation {
+        pname = "urpc";
+        version = "0.3.9";
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/uforg/uforpc/releases/download/v0.3.9/urpc-linux-amd64";
+          sha256 = "ee7364dc17bcafa4fc983649a0c8200cce0b0b96ae7b78f8a10beb0df6403193";
+        };
+
+        # Since it's a single binary, we skip the unpacking and building phases
+        phases = [ "installPhase" ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp $src $out/bin/urpc
+          chmod +x $out/bin/urpc
+        '';
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
@@ -23,6 +43,7 @@
           go-task
           git
           curl
+          urpc # RPC Framework
           librsvg # To convert SVG Logos to PNG
           imagemagick # To convert PNG Logos to ICO
           uutils-coreutils-noprefix # For sha256sum
